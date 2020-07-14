aaa330923820d3bd7cf6a9ac14c8f0d65773c88a
beingj <beingj@163.com>
2020-07-14 18:28:38 +0800

    change publicPath and title

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,14 @@ module.exports = {
         "vuetify"
     ],
     "outputDir": "docs",
+    "publicPath": process.env.NODE_ENV === 'production'
+        ? '/rawsel/'
+        : '/',
+    pages: {
+        index: {
+            entry: 'src/main.ts',
+            title: 'Raw SEL Viewer'
+        }
+    }
     // "parallel": 2 // not work
 }
